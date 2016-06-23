bc09942f5ba4aee20e0efccb265994bf76c53bfd
datatypevoid <david.r.niciforovic@gmail.com>
2016-06-22 23:14:37 -0400

    feat: working google maps example

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,116 @@
-import {bootstrap}    from '@angular/platform-browser-dynamic';
-import {AppComponent} from './app.component';
+import {
+  Component,
+  provide
+} from '@angular/core';
 
-bootstrap(AppComponent);
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import {
+  MapsAPILoader,
+  NoOpMapsAPILoader,
+  MouseEvent,
+  GOOGLE_MAPS_PROVIDERS,
+  GOOGLE_MAPS_DIRECTIVES
+} from 'angular2-google-maps/core';
+
+
+@Component({
+  selector: 'my-app',
+  directives: [GOOGLE_MAPS_DIRECTIVES],
+  styles: [`
+    .sebm-google-map-container {
+       height: 300px;
+     }
+  `],
+  template: `
+    <sebm-google-map
+      [latitude]="lat"
+      [longitude]="lng"
+      [zoom]="zoom"
+      [disableDefaultUI]="false"
+      [zoomControl]="false"
+      (mapClick)="mapClicked($event)">
+
+      <sebm-google-map-marker
+          *ngFor="let m of markers; let i = index"
+          (markerClick)="clickedMarker(m.label, i)"
+          [latitude]="m.lat"
+          [longitude]="m.lng"
+          [label]="m.label"
+          [markerDraggable]="m.draggable"
+          (dragEnd)="markerDragEnd(m, $event)">
+
+        <sebm-google-map-info-window>
+          <strong>InfoWindow content</strong>
+        </sebm-google-map-info-window>
+
+      </sebm-google-map-marker>
+
+      <sebm-google-map-circle [latitude]="lat + 0.3" [longitude]="lng"
+          [radius]="5000"
+          [fillColor]="'red'"
+          [circleDraggable]="true"
+          [editable]="true">
+      </sebm-google-map-circle>
+
+    </sebm-google-map>
+`})
+export class App {
+  // google maps zoom level
+  zoom: number = 8;
+
+  // initial center position for the map
+  lat: number = 51.673858;
+  lng: number = 7.815982;
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
+
+  mapClicked($event: MouseEvent) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng
+    });
+  }
+
+  markerDragEnd(m: marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+
+  markers: marker[] = [
+	  {
+		  lat: 51.673858,
+		  lng: 7.815982,
+		  label: 'A',
+		  draggable: true
+	  },
+	  {
+		  lat: 51.373858,
+		  lng: 7.215982,
+		  label: 'B',
+		  draggable: false
+	  },
+	  {
+		  lat: 51.723858,
+		  lng: 7.895982,
+		  label: 'C',
+		  draggable: true
+	  }
+  ]
+}
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable?: boolean;
+}
+
+// ANGULAR2_GOOGLE_MAPS_PROVIDERS is required here
+bootstrap(App, [
+  GOOGLE_MAPS_PROVIDERS,
+  // If you don't want to let angular2-google-maps load the Google Maps API script,
+  // you can use the NoOpMapsAPILoader like this:
+  // provide(MapsAPILoader, {useClass: NoOpMapsAPILoader})
+])
